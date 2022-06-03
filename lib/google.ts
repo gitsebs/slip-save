4ab82699e53f459569d88d27d3c393bd15237cd3
Sebastian Scholten <scholten.seb@gmail.com>
2022-06-03 23:08:31 +1000

    refactor

--- lib/google.ts
+++ lib/google.ts
@@ -6,16 +6,16 @@ import { Credentials } from "google-auth-library/build/src/auth/credentials";
 const { client_secret, client_id, redirect_uris } =
   require("../credentials.json").installed;
 
-const tokenPath = path.join(__dirname, "../", "tokens.json");
+export const auth = new google.auth.OAuth2({
+  clientId: client_id,
+  clientSecret: client_secret,
+  redirectUri: redirect_uris[0],
+});
 
-export const auth = new google.auth.OAuth2(
-  client_id,
-  client_secret,
-  redirect_uris[0]
-);
+const TOKEN_FILE = path.join(__dirname, "../", "tokens.json");
 
 export const getGmailService = async () => {
-  const token = await fs.readFile(tokenPath, "utf8");
+  const token = await fs.readFile(TOKEN_FILE, "utf8");
   auth.on("tokens", saveTokens);
   auth.setCredentials(JSON.parse(token));
   const gmail = google.gmail({ version: "v1", auth: auth });
@@ -23,6 +23,6 @@ export const getGmailService = async () => {
 };
 
 export async function saveTokens(token: Credentials) {
-  await fs.writeFile(tokenPath, JSON.stringify(token));
+  await fs.writeFile(TOKEN_FILE, JSON.stringify(token));
   console.log("Access token and refresh token stored to token.json");
 }
